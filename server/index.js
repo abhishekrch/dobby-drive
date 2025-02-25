4afaa82a1e3b83ddc5edc01509021a23cf6036d3
Abhishek Choudhary <pmoabhishek@gmail.com>
2025-02-25 21:51:22 +0530

    fix(deploy): resolve issues in vercel.json

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: 'https://dobby-drive-c.vercel.app', 
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', 
   credentials: true, 
   optionsSuccessStatus: 204, 
   allowedHeaders: ['Content-Type', 'Authorization']
